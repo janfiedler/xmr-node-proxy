f3816a5301294bedf2ced20746c3d8ffbb663b08
Jan Fiedler <janfiedler@gmail.com>
2018-10-16 20:55:25 +0200

    need solve Error: SQLITE_BUSY: database is locked errno: 5, code: 'SQLITE_BUSY'

--- proxy.js
+++ proxy.js
@@ -1140,6 +1140,7 @@ async function handleMinerData(method, params, ip, portData, sendReply, pushMess
      */
     let miner = activeMiners[params.id];
     // Check for ban here, so preconnected attackers can't continue to screw you
+    /*
     let isBan = await sql.isBan(ip);
     if(isBan.s) {
         if(Date.now() < isBan.r.end) {
@@ -1150,6 +1151,7 @@ async function handleMinerData(method, params, ip, portData, sendReply, pushMess
             console.log("Miner under IP " +ip+ " was unbanned.")
         }
     }
+    */
 
     switch (method) {
         case 'login':
@@ -1245,8 +1247,8 @@ async function handleMinerData(method, params, ip, portData, sendReply, pushMess
             let shareAccepted = miner.coinFuncs.processShare(miner, job, blockTemplate, params.nonce, params.result);
 
             if (!shareAccepted) {
-                sql.addBan(miner.ip, Date.now()+43200000);
-                console.error("Miner " + miner.user + " under IP " + miner.ip + " was banned!");
+                //sql.addBan(miner.ip, Date.now()+43200000);
+                //console.error("Miner " + miner.user + " under IP " + miner.ip + " was banned!");
                 sendReply('Low difficulty share');
                 return;
             }
@@ -1691,9 +1693,11 @@ if (cluster.isMaster) {
         clusterMasterInit();
     })();
 } else {
+    /*
     (async function () {
         await sql.connect();
     })();
+    */
     /*
     setInterval(checkAliveMiners, 30000);
     setInterval(retargetMiners, global.config.pool.retargetTime * 1000);
